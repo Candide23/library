6f6df7ad236d8a08c8b0c581d2c9d58550c7613f
Cham Mboungou-kimpolo <candide@Chams-MacBook-Pro.local>
2023-03-20 13:40:42 -0500

    added react routing

--- src/main/ui/src/App.tsx
+++ src/main/ui/src/App.tsx
@@ -1,21 +1,32 @@
 import React from 'react';
 import './App.css';
-import { Carousel } from './layouts/HomePage/components/Carousel';
-import { ExploreTopBooks } from './layouts/HomePage/components/ExploreTopBooks';
-import { Heros } from './layouts/HomePage/components/Heros';
-import { LibraryServices } from './layouts/HomePage/components/LibraryServices';
+import { HomePage } from './layouts/HomePage/HomePage';
 import { Footer } from './layouts/NavBarAndFooter/Footer';
 import { Navbar } from './layouts/NavBarAndFooter/Navbar';
+import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+import { Route, Switch, Redirect } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <Navbar/>
-      <ExploreTopBooks/>
-      <Carousel/>
-      <Heros/>
-      <LibraryServices/>
-      <Footer/>   
+    <div className='d-flex flex-column min-vh-100'>
+      <Navbar />
+      <div className='flex-grow-1'>
+
+      <Switch>
+        <Route path='/' exact>
+          <Redirect to='/home' />
+        </Route>
+        <Route path='/home' >
+          <HomePage />
+        </Route>
+        <Route path='/search'>
+          <SearchBooksPage />
+        </Route>
+      </Switch>
+      </div>
+
+      <Footer />
+
     </div>
   );
 }
