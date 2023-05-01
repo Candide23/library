d267bc491efa3d7f31b281eb8e6a95eac959852c
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2023-05-01 10:46:22 -0500

    updated home page

--- src/main/ui/src/layouts/HomePage/components/ExploreTopBooks.tsx
+++ src/main/ui/src/layouts/HomePage/components/ExploreTopBooks.tsx
@@ -6,8 +6,14 @@ export const ExploreTopBooks = () => {
             <div className='container-fluid py-5 text-white 
                 d-flex justify-content-center align-items-center'>
                 <div>
-                    <h1 className='display-5 fw-bold'>Find your next adventure</h1>
-                    <p className='col-md-8 fs-4'>Where would you like to go next?</p>
+                    <h1 className='display-5 fw-bold'>Find Your Next Adventure - Where would you like to go next?</h1>
+                    <p className='col-md-8 fs-4'>
+                      </p>
+                    <p className='col-md-8 fs-4'>
+                    <p className='col-md-8 fs-4'>  For Sign In</p>
+                    <p>User ID: testuser@email.com </p>
+                    <p> Password : Application1  </p>
+                    </p>
                     <Link type='button' className='btn main-color btn-lg text-white' to='/search'>
                         Explore top books</Link>
                 </div>
