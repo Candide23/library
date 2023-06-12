5733bfb541a20107b28787ada55703211d1233e1
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2023-06-12 16:24:37 -0500

    still have error with CORS

--- src/main/ui/src/lib/oktaConfig.ts
+++ src/main/ui/src/lib/oktaConfig.ts
@@ -1,6 +1,7 @@
 export const oktaConfig = {
     clientId: '0oa8xqwronWgsF5Ov5d7',
-    issuer: 'https://dev-15777272.okta.com/oauth2/default',
+    issuer: 'http://librarymk.us-east-2.elasticbeanstalk.com/oauth2/default',
+    //issuer: 'https://dev-15777272.okta.com/oauth2/default',
     //redirectUri:'http://localhost:3000/login/callback',
     redirectUri:'http://librarymk.s3-website.us-east-2.amazonaws.com/login/callback',
     scopes: ['openid','profile','email'],
