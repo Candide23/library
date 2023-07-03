2d9037d22d428dcd25dfa294ff84aa73da393c2a
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2023-07-03 11:06:51 -0500

    trying to deploy aws

--- src/main/ui/src/lib/oktaConfig.ts
+++ src/main/ui/src/lib/oktaConfig.ts
@@ -1,9 +1,9 @@
 export const oktaConfig = {
     clientId: '0oa8xqwronWgsF5Ov5d7',
-    issuer: 'http://librarymk.us-east-2.elasticbeanstalk.com/oauth2/default',
-    //issuer: 'https://dev-15777272.okta.com/oauth2/default',
-    //redirectUri:'http://localhost:3000/login/callback',
-    redirectUri:'http://librarymk.s3-website.us-east-2.amazonaws.com/login/callback',
+    issuer: 'https://dev-15777272.okta.com/oauth2/default',
+    //issuer: 'http://librarymk.s3-website.us-east-2.amazonaws.com',
+    redirectUri:'http://localhost:3000/login/callback',
+    //redirectUri:'http://librarymk.s3-website.us-east-2.amazonaws.com/login/callback',
     scopes: ['openid','profile','email'],
     pkce: true,
     disableHttpsCheck: true,
